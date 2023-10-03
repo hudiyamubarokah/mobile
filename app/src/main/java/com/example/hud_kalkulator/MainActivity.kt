b9980dc470a13b9a861c7bdf17a8535acd7e20df
hudiyamubarokah <hudiyamubarokah@gmail.com>
2023-10-03 10:04:22 +0700

    hudiya make

--- app/src/main/java/com/example/hud_kalkulator/MainActivity.kt
+++ app/src/main/java/com/example/hud_kalkulator/MainActivity.kt
@@ -1,52 +1,42 @@
 package com.example.hud_kalkulator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Telephony.Mms.Intents
 import android.view.View
 import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
 
-    private lateinit var edt_length: EditText
-    private lateinit var edt_width: EditText
-    private lateinit var edt_height: EditText
-    private lateinit var btn_result: Button
-    private lateinit var result: TextView
-
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var btn_switch: Button
+    private lateinit var btn_data: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        edt_length = findViewById(R.id.edt_length)
-        edt_width = findViewById(R.id.edt_width)
-        edt_height = findViewById(R.id.edt_height)
-        btn_result = findViewById(R.id.btn_result)
-        result = findViewById(R.id.result)
-        btn_result.setOnClickListener {
+        btn_switch = findViewById(R.id.btn_switch)
+        btn_data= findViewById(R.id.btn_data)
+        btn_switch.setOnClickListener(this)
+        btn_data.setOnClickListener(this)
 
-            var panjang = edt_length.text.toString().trim()
-            var lebar = edt_width.text.toString().trim()
-            var tinggi = edt_height.text.toString().trim()
 
-            var isEmpty = false
-            if (panjang.isEmpty()) {
-                isEmpty = true
-                edt_length.error = "wajib diisi"
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id){
+            R.id.btn_switch->{
+                val moveIn = Intent(this@MainActivity, activity_move::class.java)
+                startActivity(moveIn)
             }
-            if (lebar.isEmpty()) {
-                isEmpty = true
-                edt_width.error = "wajib diisi"
-            }
-            if (tinggi.isEmpty()) {
-                isEmpty = true
-                edt_height.error = "wajib diisi"
-            }
-            if (!isEmpty) {
-                var volume = panjang.toDouble() * lebar.toDouble() * tinggi.toDouble()
-                result.text = volume.toString()
+            R.id.btn_data->{
+                val intentData = Intent(this@MainActivity, activity_move_data::class.java)
+                intentData.putExtra(activity_move_data.EXTRA_NAME, "hudiya")
+                intentData.putExtra(activity_move_data.EXTRA_AGE, 20)
+
+                startActivity(intentData)
             }
         }
     }
+
 }
---
+++ app/src/main/java/com/example/hud_kalkulator/activity_move.kt
@@ -0,0 +1,11 @@
+package com.example.hud_kalkulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_move : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move)
+    }
+}
